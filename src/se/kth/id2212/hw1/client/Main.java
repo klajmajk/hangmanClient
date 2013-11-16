789abba5ca5e102f62b5393434ed54bc87fef7f2
Adam <adaklima@gmail.com>
2013-11-16 11:56:43 +0100

    simple testing

--- src/se/kth/id2212/hw1/client/Main.java
+++ src/se/kth/id2212/hw1/client/Main.java
@@ -1,32 +1,44 @@
 package se.kth.id2212.hw1.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Lucas
  */
 public class Main {
 
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.startNewGame();
-        controller.guessLetter('e');
-        controller.guessLetter('n');
-        controller.guessWord("car");
-        controller.guessWord("doodle");
-        controller.guessLetter('z');
-        controller.guessLetter('z');
-        controller.guessLetter('z');
-        controller.guessLetter('z');
-        controller.guessLetter('z');
-        controller.guessLetter('z');
-        
-        controller.startNewGame();
+        while (true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String input;
+                input = br.readLine();
+                controller.guessLetter(input.charAt(0));
+
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+//        controller.startNewGame();
+//        controller.guessLetter('e');
+//        controller.guessLetter('n');
+//        controller.guessWord("car");
+//        controller.guessWord("doodle");
+//        controller.guessLetter('z');
+//        controller.guessLetter('z');
+//        controller.guessLetter('z');
+//        controller.guessLetter('z');
+//        controller.guessLetter('z');
+//        controller.guessLetter('z');
+//
+//        controller.startNewGame();
     }
-
- 
-
 }
